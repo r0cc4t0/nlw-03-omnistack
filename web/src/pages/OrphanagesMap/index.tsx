51db5144d5df3ad6b7931b425cbc8340ea51591e
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-13 22:30:40 -0300

    Web Application Map Integration

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import './styles.css';
+import mapMarkerImg from '../../assets/images/map-marker.svg';
 
 const OrphanagesMap = () => {
   return (
-    <h1>Orphanages Map</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa.</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+
+      <MapContainer
+        center={[-27.2092052, -49.6401092]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
   );
 };
 
