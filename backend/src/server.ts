453c4ad4420ac397c044c0ed81f811ecba8ce794
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-14 16:28:28 -0300

    Database Creation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
 
@@ -6,33 +7,6 @@ app.use(express.json());
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello, World!' });
-});
-
-const users = [
-  'Ana', 'William', 'Brenda', 'Gabriel', 'Micaelli', 'Bruno'
-];
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-  const filteredUsers = search
-    ? users.filter(user => user.includes(search))
-    : users;
-  return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-  const user = {
-    name: data.name,
-    email: data.email
-  };
-  return response.json(user);
 });
 
 app.listen(3333);
