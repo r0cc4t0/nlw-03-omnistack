f933f3264f9937f1493125d6c328ce9ad59ef10f
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-15 23:21:13 -0300

    Orphanage Registration

--- backend/src/database/migrations/1615748308019-create_orphanages.ts
+++ backend/src/database/migrations/1615748308019-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1615748308019 implements MigrationInterface {
           type: 'text'
         },
         {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
