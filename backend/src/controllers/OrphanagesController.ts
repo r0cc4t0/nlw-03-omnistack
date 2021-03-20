0a11beafa39af06295be54bb7639cf6751e49601
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-19 22:07:01 -0300

    Data Validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 import Orphanage from '../models/Orphanage';
 import OrphanagesView from '../views/OrphanagesView';
 
@@ -47,7 +48,7 @@ export default {
       };
     });
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -56,10 +57,31 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      ).required().min(1)
     });
+
+    await schema.validate(data, {
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
-    return response.status(201).json(orphanage);
+    return response.status(201).json(OrphanagesView.render(orphanage));
   }
 };
