d7ace78040ec0c042427f7eee8dbf550d0a9c72b
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-23 22:20:50 -0300

    Orphanage Details

--- web/src/pages/Orphanage/index.tsx
+++ web/src/pages/Orphanage/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 import { FiClock, FiInfo } from 'react-icons/fi';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import Leaflet from 'leaflet';
+import { useParams } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 import Sidebar from '../../components/Sidebar';
 import mapMarkerImg from '../../assets/images/map-marker.svg';
 
@@ -14,55 +16,72 @@ const happyMapIcon = Leaflet.icon({
   popupAnchor: [0, -60]
 });
 
+interface OrphanageDetails {
+  name: string;
+  latitude: number;
+  longitude: number;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    id: number;
+    url: string;
+  }>;
+};
+
+interface OrphanageParams {
+  id: string;
+};
+
 const Orphanage = () => {
+  const params = useParams<OrphanageParams>();
+  const [orphanage, setOrphanage] = useState<OrphanageDetails>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  useEffect(() => {
+    api.get(`orphanages/${params.id}`).then(response => {
+      setOrphanage(response.data);
+    });
+  }, [params.id]);
+
+  if (!orphanage) {
+    return (
+      <p>Carregando...</p>
+    );
+  }
+
   return (
     <div id="page-orphanage">
       <Sidebar />
 
       <main>
         <div className="orphanage-details">
-          <img
-            src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-            alt="Lar das Meninas"
-          />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
+
           <div className="images">
-            <button type="button" className="active">
-              <img
-                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-                alt="Lar das Meninas"
-              />
-            </button>
-            <button type="button">
-              <img
-                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-                alt="Lar das Meninas"
-              />
-            </button>
-            <button type="button">
-              <img
-                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-                alt="Lar das Meninas"
-              />
-            </button>
-            <button type="button">
-              <img
-                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-                alt="Lar das Meninas"
-              />
-            </button>
-            <button type="button">
-              <img
-                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe-image.jpg"
-                alt="Lar das Meninas"
-              />
-            </button>
+            {orphanage.images.map((image, index) => {
+              return (
+                <button
+                  key={image.id}
+                  type="button"
+                  className={activeImageIndex === index ? 'active' : ''}
+                  onClick={() => {
+                    setActiveImageIndex(index);
+                  }}
+                >
+                  <img src={image.url} alt={orphanage.name} />
+                </button>
+              );
+            })}
           </div>
+
           <div className="orphanage-details-content">
-            <h1>Lar da Meninas</h1>
-            <p>Presta assistência a crianças de 6 a 15 anos que se encontrem em situação de risco e/ou vulnerabilidade.</p>
+            <h1>{orphanage.name}</h1>
+            <p>{orphanage.about}</p>
             <div className="map-container">
               <MapContainer
-                center={[-27.2092052, -49.6401092]}
+                center={[orphanage.latitude, orphanage.longitude]}
                 zoom={15}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -78,24 +97,37 @@ const Orphanage = () => {
                 <Marker
                   interactive={false}
                   icon={happyMapIcon}
-                  position={[-27.2092052, -49.6401092]}
+                  position={[orphanage.latitude, orphanage.longitude]}
                 />
               </MapContainer>
+              <footer>
+                <a target="_blank" rel="noopener noreferrer" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
+              </footer>
             </div>
+
             <hr />
+
             <h2>Instruções para a visita</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            <p>{orphanage.instructions}</p>
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15b6d6" />
-                Segunda à Sexta <br />
-                08:00 às 18:00
+                Segunda à Sexta<br />{orphanage.opening_hours}
               </div>
-              <div className="open-on-weekends">
-                <FiInfo size={32} color="#39cc83" />
-                Atendemos <br />
-                fim de semana
-              </div>
+              {orphanage.open_on_weekends
+                ? (
+                  <div className="open-on-weekends">
+                    <FiInfo size={32} color="#39cc83" />
+                    Atendemos<br />fim de semana
+                  </div>
+                )
+                : (
+                  <div className="closed-on-weekends">
+                    <FiInfo size={32} color="#ff669d" />
+                    Não atendemos<br />fim de semana
+                  </div>
+                )
+              }
             </div>
             <button type="button" className="contact-button">
               <FaWhatsapp size={20} color="#fff" />
