3298556f057dc8809c24dbb4220b29582662f096
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-28 14:01:14 -0300

    Mobile Application Structure with React Native

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, World!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: '#ffffff',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+});
