f72588553ad74cb4592a776669dfc0ebe64978b9
r0cc4t0 <guilherme.roccato@outlook.com>
2021-03-30 21:25:19 -0300

    Mobile Application Navigation Settings

---
+++ mobile/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import mapMarker from '../../assets/images/map-marker.png';
+import styles from './styles';
+
+const OrphanagesMap = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToOrphanageDetails() {
+    navigation.navigate('OrphanageDetails');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.75,
+            y: 0.85
+          }}
+          coordinate={{
+            latitude: -27.2092052,
+            longitude: -49.6401092
+          }}
+        >
+          <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+          <Feather name="plus" size={20} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default OrphanagesMap;
