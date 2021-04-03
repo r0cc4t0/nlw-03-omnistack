c621ddb11c0f73ee7058709df971704d49c37532
r0cc4t0 <guilherme.roccato@outlook.com>
2021-04-02 22:45:24 -0300

    Mobile Application Pages Styles

--- mobile/src/pages/SelectMapPosition/styles.ts
+++ mobile/src/pages/SelectMapPosition/styles.ts
@@ -1,10 +1,33 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
-  mapStyle: {},
-  nextButton: {},
-  nextButtonText: {}
+  container: {
+    flex: 1,
+    position: 'relative'
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#fff'
+  }
 });
 
 export default styles;
